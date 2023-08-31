2f6102fec848cc4e42a447aee53a486048efd0cf
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2023-08-30 22:58:03 -0300

    Update Funciones.sql

--- Queries/Funciones.sql
+++ Queries/Funciones.sql
@@ -11,16 +11,16 @@ DELIMITER ;
 
 -- Función para contar clientes con tarjetas de crédito, débito u otros medios de pago
 DELIMITER //
-CREATE FUNCTION CountVisaAndAmexCards() RETURNS VARCHAR(255)
+CREATE FUNCTION ContadorVisaYAmex() RETURNS VARCHAR(255)
 BEGIN
-    DECLARE visa_count INT;
-    DECLARE amex_count INT;
+    DECLARE visa INT;
+    DECLARE amex INT;
     DECLARE result_message VARCHAR(255);
     
-    SELECT COUNT(*) INTO visa_count FROM metodo_pago WHERE tipo_tarjeta = 'visa';
-    SELECT COUNT(*) INTO amex_count FROM metodo_pago WHERE tipo_tarjeta = 'amex';
+    SELECT COUNT(*) INTO visa FROM metodo_pago WHERE tipo_tarjeta = 'visa';
+    SELECT COUNT(*) INTO amex FROM metodo_pago WHERE tipo_tarjeta = 'amex';
     
-    SET result_message = CONCAT('Tarjetas Visa: ', CAST(visa_count AS CHAR), ', Tarjetas Amex: ', CAST(amex_count AS CHAR));
+    SET result_message = CONCAT('Tarjetas Visa: ', CAST(visa AS CHAR), ', Tarjetas Amex: ', CAST(amex AS CHAR));
     
     RETURN result_message;
 END;
